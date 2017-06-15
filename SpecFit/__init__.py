703a2218d6b85f01831502a0a113561e6b6459d6
Szymon Prajs <S.Prajs@soton.ac.uk>
2017-06-15 22:03:31 +0100

    Commit old code, this needs to be refactored

---
+++ SpecFit/__init__.py
@@ -0,0 +1,1 @@
+"""Package for fitting spectral features and comparing spectra"""
---
+++ SpecFit/utils.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def rebin_spectrum(wavelength, flux, wavlength_bin):
+    """
+    Rebin spectrum to a new wavelength bin
+
+    Parameters
+    ----------
+    wavelength : array-like
+        wavelength array
+    flux : array-like
+        flux array
+    wavlength_bin : float
+        size of the new wavelength bin
+
+    Returns
+    ----------
+    x_new : numpy.ndarray
+        Array of new rebinned wavelengths
+    y_new : numpy.ndarray
+        Array of rebinned fluxes
+    """
+    wavelength = np.array(wavelength).astype(float)
+    flux = np.array(flux).astype(float)
+    wavlength_bin = float(wavlength_bin)
+
+    x_new = np.arange(wavelength.min() - wavlength_bin/2, wavelength.max(), wavlength_bin)
+    y_new = np.zeros(x_new.size)
+    flat = flux[0]
+
+    for i in range(x_new.size):
+        idx = np.where((wavelength > x_new[i]) & (wavelength < x_new[i]+wavlength_bin))
+        y_new[i] = np.sum(flux[idx])
+        div = idx[0].size
+        if idx[0].size > 0:
+            if (idx[0].min() > 0) and (idx[0].max() < (wavelength.size - 1)):
+                y_new[i] += ((wavelength[idx].min() - x_new[i])/wavlength_bin) * flux[idx[0].min()-1]
+                y_new[i] += ((x_new[i] + wavlength_bin - wavelength[idx].max())/wavlength_bin) * flux[idx[0].max()+1]
+                div += (wavelength[idx].min() - x_new[i])/wavlength_bin
+                div += (x_new[i] + wavlength_bin - wavelength[idx].max())/wavlength_bin
+        if div == 0:
+            y_new[i] = flat
+        else:
+            y_new[i] /= div
+            flat = y_new[i]
+
+    return x_new, y_new
